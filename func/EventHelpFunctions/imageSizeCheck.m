caeed16ab69e85ee2d95a56813106b3454526778
Shanshi <etteerr@gmail.com>
2016-12-02 11:36:29 +0100

    Fixed faulty image resize
    Now using aspectRatio's

--- func/EventHelpFunctions/imageSizeCheck.m
+++ func/EventHelpFunctions/imageSizeCheck.m
@@ -4,17 +4,22 @@ function [ img, imgSize, didResize ] = imageSizeCheck(img, windowPtr )
 
 %% Initial settings
 [width, height]=Screen('WindowSize', windowPtr );
-[h, w] = size(img);
+[h, w, ~] = size(img);
 imgSize = [w,h];
+aspectRatioScreen = width/height;
+aspectRatioImage  = w/h;
 didResize = false;
 %% Check and if too big, do resize. Keep aspect ratio
-if (w > width) || (h > height)
-    if (w>h)
+if (w>width || h>height)
+    if (aspectRatioImage > aspectRatioScreen)
         img = imresize(img, [NaN width]);
     else
         img = imresize(img, [height NaN]);
     end
     didResize = true;
+    [h, w, ~] = size(img);
+    imgSize = [w,h];
 end
+
 end
 
